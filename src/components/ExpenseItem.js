25f4565b3c387f2bf6b16075b1814dfe8ee755ae
Anibal Vielma <asenciovd@gmail.com>
2023-09-12 13:12:40 -0400

    evaluation

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,12 @@
-import React, { useContext } from 'react';
-import { TiDelete } from 'react-icons/ti';
+import React, {useContext} from 'react';
 import { AppContext } from '../context/AppContext';
+import {TiDelete} from 'react-icons/ti';
+import {FaMinusCircle, FaPlusCircle} from 'react-icons/fa';
 
-const ExpenseItem = (props) => {
-    const { dispatch } = useContext(AppContext);
+const ExpenseItem = (props)=> {
+    const {dispatch, currency} = useContext(AppContext);
 
-    const handleDeleteExpense = () => {
+    const handleDeleteExpense = ()=> {
         dispatch({
             type: 'DELETE_EXPENSE',
             payload: props.id,
@@ -14,23 +15,36 @@ const ExpenseItem = (props) => {
 
     const increaseAllocation = (name) => {
         const expense = {
-            name: name,
-            cost: 10,
+            name:name, 
+            cost:10,
         };
 
         dispatch({
-            type: 'ADD_EXPENSE',
-            payload: expense
+            type:'ADD_EXPENSE',
+            payload: expense,
         });
+    }
 
+    const decreaseAllocation = (name) => {
+        const expense = {
+            name:name, 
+            cost:10,
+        };
+
+        dispatch({
+            type:'RED_EXPENSE',
+            payload: expense,
+        });
     }
 
     return (
         <tr>
-        <td>{props.name}</td>
-        <td>£{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
-        <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
+            <td>{props.name}</td>
+            <td>{currency}{props.cost}</td>
+            <td><FaPlusCircle size='1.9em' color="Green" onClick={event => increaseAllocation(props.name)}></FaPlusCircle></td>
+            <td><FaMinusCircle size='1.9em' color="Red" onClick={event => decreaseAllocation(props.name)}></FaMinusCircle></td>
+            <td><TiDelete size='2.2em' color="red" onClick={handleDeleteExpense}></TiDelete></td>
+
         </tr>
     );
 };
--- src/components/ExpenseTotal.js
+++ src/components/ExpenseTotal.js
@@ -1,15 +1,17 @@
-
-import React, { useContext } from 'react';
+import React, {useContext} from 'react';
 import { AppContext } from '../context/AppContext';
+
 const ExpenseTotal = () => {
-    const { expenses } = useContext(AppContext);
-    const totalExpenses = expenses.reduce((total, item) => {
-        return (total += item.cost);
+    const {expenses, currency} = useContext(AppContext);
+    const totalExpenses = expenses.reduce((total, item) =>{
+        return (total = total + item.cost);
     }, 0);
-    return (
-        <div className='alert alert-primary'>
-            <span>Spent so far: £{totalExpenses}</span>
+
+    return(
+        <div className = 'alert alert-primary'>
+            <span>Spent so far: {currency}{totalExpenses}</span>
         </div>
     );
 };
+
 export default ExpenseTotal;
